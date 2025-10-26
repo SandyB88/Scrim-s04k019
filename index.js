675ff309654e6de7cb9480dde7596fe5e1d0d501
SandyB88 <sanderb1988@hotmail.com>
2025-10-26 19:58:33 +0100

    commit

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-let homePlusOne = document.getElementById("hone")
-let homeScore = document.getElementById("homescore")
-let homeCount = 0 
+let hScore = 0 
+document.getElementById("homescore").textContent = hScore
 
+let gScore = 0
+document.getElementById("guestscore").textContent = gScore
 
-function homePlusOne() {
-  homeCount += 1
-  console.log(homeCount)
-  homeScore.textContent = homeCount
+function hOne() {
+  hScore += 1
+  document.getElementById("homescore").textContent = hScore
+  console.log("One point added! Total Score: " + hScore)
 }
 
+function hTwo() {
+  hScore += 2
+  document.getElementById("homescore").textContent = hScore
+  console.log("Two points added! Total Score: " + hScore)
+}
+
+function hThree() {
+  hScore += 3
+  document.getElementById("homescore").textContent = hScore
+  console.log("Three points added! Total Score: " + hScore)
+}
+
+function gOne() {
+  gScore += 1
+  document.getElementById("guestscore").textContent = gScore
+  console.log("One point added! Total Score: " + gScore)
+}
+
+function gTwo() {
+  gScore += 2
+  document.getElementById("guestscore").textContent = gScore
+  console.log("Two points added! Total Score: " + gScore)
+}
+
+function gThree() {
+  gScore += 3
+  document.getElementById("guestscore").textContent = gScore
+  console.log("Three points added! Total Score: " + gScore)
+}
+
+
+function reset() {
+  hScore = 0
+  gScore = 0  
+  document.getElementById("homescore").textContent = hScore
+  document.getElementById("guestscore").textContent = gScore
+  console.log("Restart the count!!!!")
+}
